2e9ab8fe2de01e1330e24d3a56f87286a46bfce7
Erin Rivas <erin@atomicbear.com>
2024-04-10 17:27:48 -0500

    literal trash

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
+  <button @click="attempt">Listen</button>
   <router-view />
 </template>
 
 <script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import { IbcSigningStargateClient, IIbcSigningStargateClient, TMergedSigner } from '@jackallabs/banshee'
 
+  const banshee = ref<IIbcSigningStargateClient | null>(null)
+
+  onMounted(async () => {
+    if (!window.keplr) {
+      throw new Error(' No keplr')
+    }
+    const signer = (await window.keplr.getOfflineSignerAuto(
+      'jackal-1',
+    )) as TMergedSigner
+
+    banshee.value = await IbcSigningStargateClient.connectWithSigner(
+      'http://localhost:8080',
+      signer,
+    )
+  })
+
+  function attempt() {
+    if (banshee.value) {
+      banshee.value.test('ws://localhost:59799')
+    } else {
+      console.warn('No banshee')
+    }
+  }
 </script>
 
 <style lang="scss">
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+import { Window as KeplrWindow } from '@keplr-wallet/types'
+
+declare global {
+  interface Window extends KeplrWindow, LeapWindow {}
+}
